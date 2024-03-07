c901e9a869b20e68188e362b035812caa0bd551a
Mathialahan-U <97272626+Mathialahan-U@users.noreply.github.com>
2024-03-07 23:47:54 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import{AiOutlineSearch} from 'react-icons/ai'
-import{FaMoon} from 'react-icons/fa'
-import {useSelector} from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
+    const { theme } = useSelector((state) => state.theme);
     const {currentUser} = useSelector(state => state.user)
   return (
     <Navbar className='border-b-2'>
@@ -25,8 +28,13 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='black' pill>
-                <FaMoon/>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
             </Button>
             {currentUser ? (
                 <Dropdown 
@@ -42,7 +50,9 @@ export default function Header() {
                 >
                     <Dropdown.Header>
                         <span className='block text-sm'>@{currentUser.username}</span>
-                        <span className='block text-sm front-medium truncate'>@{currentUser.email}</span>
+                        <span className='block text-sm font-medium truncate'>
+                            {currentUser.email}
+                        </span>
                     </Dropdown.Header>
                     <Link to={'/dashboard?tab=profile'}>
                         <Dropdown.Item>Profile</Dropdown.Item>
