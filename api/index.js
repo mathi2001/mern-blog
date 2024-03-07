7ad3e6f47d192424ec5d9e633ec89fe2438a0620
Mathialahan-U <97272626+Mathialahan-U@users.noreply.github.com>
2024-03-07 12:13:32 +0530

    create sign api route

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
     res.status(statusCode).json({
-        succsee: false,
+        success: false,
         statusCode,
         message
     });
